0d50481fa8584da0ba51e523483fc1a28d632075
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-13 12:53:09 -0700

    may add/change some things later

---
+++ breedFetcher.js
@@ -0,0 +1,36 @@
+const request = require('request');
+// https://api.thecatapi.com/v1/breeds/search
+
+const breedFetcher = (breed, callback) => {
+  //make search url variable so that its less messy code
+  let search = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(search, (error, response, body) => {
+    /*
+    ***dont need these for now
+    console.log(typeof body);
+    console.log(body);
+    */
+
+    if (error) {
+      callback(`ERROR: ${error}`, null);
+    }
+
+    //first data entry: breed info, make variable
+    const data = JSON.parse(body);
+    //first entry at index 0
+    const breedInfo = data[0];
+    
+    //if no description, its invalid
+    if (breedInfo.length === 0) { 
+      callback('Invalid breed');
+    }
+
+    if (breedInfo) {
+      callback(null, breedInfo.description);
+    }
+
+  });
+
+};
+
+module.exports = {breedFetcher};
